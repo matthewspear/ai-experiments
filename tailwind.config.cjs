89aebbc2e628a836473e214f29fab6f538f97712
Matt Spear <matthewtimothyspear@gmail.com>
2023-02-07 23:20:08 +0700

    Tidy up t3, layout index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
