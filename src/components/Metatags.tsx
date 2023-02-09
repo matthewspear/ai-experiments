935610ea872ce16b150569422880f95ba2575747
Matt Spear <matthewtimothyspear@gmail.com>
2023-02-09 15:20:15 +0700

    Fix metatags

---
+++ src/components/Metatags.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+export default function Metatags({
+  title,
+  description,
+  image,
+}: {
+  title: string;
+  description: string;
+  image: string;
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@matthew_spear" />
+      <meta name="twitter: title" content={title} />
+      <meta name="twitter: description" content={description} />
+      <meta name="twitter:image" content={image} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+    </Head>
+  );
+}
