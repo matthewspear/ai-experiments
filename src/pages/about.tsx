c4efd5ad9226f6b38d42ae247443deb5578a4c26
Matt Spear <matthewtimothyspear@gmail.com>
2023-02-14 18:11:35 +0700

    Fix about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -40,32 +40,32 @@ const About: NextPage = () => {
 
           <div className="prose prose-lg prose-slate mt-8 w-full">
             <p>
-              👋 Hi, I'm{" "}
+              👋 Hi, I&apos;m{" "}
               <a href="https://mattspear.co" target="_blank" rel="noreferrer">
                 Matt Spear
               </a>
-              . I'm a freelance software engineer and for the last decade I've
-              been facinated with technology, building with it and learning from
-              it.
+              . I&apos;m a freelance software engineer and for the last decade
+              I&apos;ve been facinated with technology, building with it and
+              learning from it.
             </p>
             <p>
               Since learning to program AI, Deep Learning, ML has always been an
-              illusive interest. I've spent time in the AR space, on SLAM
-              systems and working with CoreML, Apple's ML framework. But{" "}
+              illusive interest. I&apos;ve spent time in the AR space, on SLAM
+              systems and working with CoreML, Apple&apos;s ML framework. But{" "}
               <a href="https://fast.ai" target="_blank" rel="noreferrer">
                 Fast.ai
               </a>{" "}
-              and the "Deep Learning for Coders" course was my first real
-              introduction to everyday coders working on cutting edge ML. Jeremy
-              Howard is a legend and who doesn't love the tagline "Making neural
-              nets uncool again".
+              and the &quot;Deep Learning for Coders&quot; course was my first
+              real introduction to everyday coders working on cutting edge ML.
+              Jeremy Howard is a legend and who doesn&apos;t love the tagline
+              &quot;Making neural nets uncool again&quot;.
             </p>
             <p>
               The good news is I still feel like a beginner and this site is a
-              result of wanting to learn, share and try something new. I'm eagar
-              to build a SaaS business / App / Startup and I want to take you
-              with me along the way! In the coming weeks I plan on starting a
-              YouTube channel and creating some video content and building out
+              result of wanting to learn, share and try something new. I&apos;m
+              eagar to build a SaaS business / App / Startup and I want to take
+              you with me along the way! In the coming weeks I plan on starting
+              a YouTube channel and creating some video content and building out
               usecases, tutorials and examples.
             </p>
             <p>
