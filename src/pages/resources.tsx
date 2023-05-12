8a920bb3d077a894faf0c45faeb8fff38dfc7f31
Matt Spear <matthewtimothyspear@gmail.com>
2023-05-12 16:05:41 +0800

    Add YouTube resources section

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -34,9 +34,12 @@ function Section({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col gap-4 pb-4">
-      <h1 className="text-xl font-medium">{title}</h1>
-      {children}
+    <div className="relative">
+      <div className="absolute -mt-20 h-10 md:-mt-2 md:h-0" id={title} />
+      <div className="flex flex-col gap-4 pb-4">
+        <h1 className="text-xl font-medium">{title}</h1>
+        {children}
+      </div>
     </div>
   );
 }
@@ -46,6 +49,12 @@ const Resources: NextPage = () => {
     <Layout breadcrumbs={TopLevelBreadcrumb("Resources", "/resources")}>
       <div className="flex flex-col gap-4 md:mx-auto md:w-1/2">
         <Summary title="Resources">
+          <div className="flex gap-4">
+            <a href="#Tools">Tools</a>
+            <a href="#OpenAI">OpenAI</a>
+            <a href="#YouTube">YouTube</a>
+            <a href="#Diving%20Deeper">Diving Deeper</a>
+          </div>
           <p></p>
         </Summary>
         <Section title="Tools">
@@ -55,7 +64,10 @@ const Resources: NextPage = () => {
             url="https://mattspear.gumroad.com/l/peekapp"
           />
         </Section>
-        <Section title="Open AI">
+        {/* <Section title="Code">
+          <></>
+        </Section> */}
+        <Section title="OpenAI">
           <ResourceItem
             title="OpenAI Documentation"
             description="Guide to getting started with the OpenAI API."
@@ -82,7 +94,59 @@ const Resources: NextPage = () => {
             url="https://openai.com/research/gpt-4"
           />
         </Section>
+        <Section title="YouTube">
+          <ResourceItem
+            title="AI Explained"
+            description="Covering the biggest news of the century - the arrival of smarter-than-human AI. What is happening, what might soon happen, what it means and what we can do with it."
+            url="https://www.youtube.com/@ai-explained-"
+          />
 
+          <ResourceItem
+            title="Jeremy Howard"
+            description="Deep learning is transforming the world. We are making deep learning easier to use and getting more people from all backgrounds involved through our free courses for coders, software library, cutting-edge research, and community."
+            url="https://www.youtube.com/@howardjeremyp"
+          />
+          <ResourceItem
+            title="Two Minute Papers"
+            description="What a time to be alive! Channel provides brief, accessible summaries and explanations of complex scientific papers."
+            url="https://www.youtube.com/user/keeroyz"
+          />
+          <ResourceItem
+            title="Radek Osmulski"
+            description="I'm Radek, a Senior Data Scientist at NVIDIA and a 2x Kaggle Grandmaster. I started to learn programming and Machine Learning at 29 using online resources. I create videos for those who want to do awesome things in machine learning and don't have a traditional academic background."
+            url="https://www.youtube.com/@radek_osmulski"
+          />
+          <ResourceItem
+            title="Matt Wolfe"
+            description="AI, No-Code, Tech, Futurism - I'm a tech nerd and talk about tech nerd stuff"
+            url="https://www.youtube.com/@mreflow"
+          />
+          <ResourceItem
+            title="Fireship"
+            description="High-intensity ⚡ code tutorials to help you build & ship your app faster. Recently covering AI news, tutorials, and demos."
+            url="https://www.youtube.com/watch?v=0rIvB3LZiKA"
+          />
+          <ResourceItem
+            title="Lex Fridman"
+            description="Lex Fridman Podcast and other videos."
+            url="https://www.youtube.com/channel/UCSHZKyawb77ixDdsGog4iWA"
+          />
+          <ResourceItem
+            title="sentdex"
+            description="Python Programming tutorials, going further than just the basics. Learn about machine learning, finance, data analysis, robotics, web development, game development and more."
+            url="https://www.youtube.com/channel/UCfzlCWGWYyIQ0aLC5w48gBQ"
+          />
+          <ResourceItem
+            title="Nerdy Rodent"
+            description="Showcasing AI Stuff since 2020, including such epics as: Stable Diffusion, VQGAN, StyleGAN, Large Language Models, Style Transfer, Super resolution / upscaling and many other amazing AI applications."
+            url="https://www.youtube.com/@NerdyRodent"
+          />
+          <ResourceItem
+            title="enigmatic_e"
+            description="Videographer who also enjoys messing around with VFX and AI."
+            url="https://www.youtube.com/@enigmatic_e"
+          />
+        </Section>
         <Section title="Diving Deeper">
           <ResourceItem
             title="ChatGPT Prompt Engineering for Developers"
