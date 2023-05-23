7bffe856a22f6d99df26e5e11380ec8ec04bac1e
Matt Spear <matthewtimothyspear@gmail.com>
2023-05-23 14:12:27 +0800

    Add icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,25 @@ class MyDocument extends Document {
     return (
       <Html className="h-full scroll-smooth bg-slate-100 antialiased" lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
