e25cb9c9acb6c23d9b9aa7802b4d598213fd143d
Matt Spear <matthewtimothyspear@gmail.com>
2024-04-01 00:07:44 +0300

    Fix profile button / page

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,20 +1,54 @@
 "use client";
 
+import { BreadcrumbBuilder } from "@/components/breadcrumb-builder";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { api } from "@/trpc/react";
+import { useClerk } from "@clerk/nextjs";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
-export default function Dashboard() {
-  const getUser = api.user.getUser.useQuery();
+// TODO: Improve loading states / skeletons
+
+export default function Profile() {
+  const { data: user } = api.user.getUser.useQuery();
+  const { signOut } = useClerk();
+
+  const router = useRouter();
 
   return (
-    <div className="mt-8">
-      {getUser.isLoading ? (
-        "isLoading"
-      ) : (
-        <pre className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          {JSON.stringify(getUser.data, null, 2)}
-        </pre>
-      )}
-      {getUser.isError ? getUser.error.message : ""}
+    <div className="flex w-full flex-col gap-4">
+      <BreadcrumbBuilder page="Profile" />
+
+      <Avatar className="inline-block h-32 w-32 rounded-full">
+        <AvatarImage
+          src={user?.image ?? undefined}
+          width={128}
+          height={128}
+          alt="Profile picture for user"
+        />
+        <AvatarFallback className="bg-transparent">
+          <UserCircleIcon
+            strokeWidth={1}
+            className="inline-block h-32 w-32 text-slate-500"
+          />
+        </AvatarFallback>
+      </Avatar>
+
+      <h2>
+        {user?.firstName} {user?.lastName}
+      </h2>
+      <pre className="my-4 overflow-y-scroll whitespace-pre-wrap rounded-lg bg-white/50 p-4 text-sm backdrop-blur-md">
+        {JSON.stringify(user, null, 2)}
+      </pre>
+      <p>Signed in as {user?.email}</p>
+      <Button
+        variant="outline"
+        className="w-fit"
+        onClick={() => signOut(() => router.push("/"))}
+      >
+        Sign out
+      </Button>
     </div>
   );
 }
--- src/components/core/profile-button.tsx
+++ src/components/core/profile-button.tsx
@@ -4,19 +4,38 @@ import { useUser } from "@clerk/nextjs";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 
 export default function ProfileButton() {
   const { user, isLoaded } = useUser();
+
   return (
-    <Link href="/profile">
-      {!isLoaded && <Skeleton className="h-8 w-8 rounded-full" />}
+    <Link href="/profile" className="flex w-full items-center">
+      {!isLoaded && (
+        <div className="flex w-full items-center gap-2">
+          <Skeleton className="h-9 w-9 rounded-full" />
+          <Skeleton className="h-8 grow rounded-lg" />
+        </div>
+      )}
+
       {isLoaded && (
-        <Avatar className="h-8 w-8">
+        <Avatar className="h-9 w-9">
           <AvatarImage src={user?.imageUrl} />
-          <AvatarFallback>
-            <Skeleton className="h-8 w-8 rounded-full" />
+          <AvatarFallback className="bg-transparent">
+            <UserCircleIcon className="inline-block h-9 w-9 text-slate-500" />
           </AvatarFallback>
         </Avatar>
+      )}
+
+      {isLoaded && (
+        <div className="ml-3 flex flex-col text-left font-medium">
+          <p className="text-sm text-gray-700 group-hover:text-gray-900">
+            {user ? user?.fullName : "User"}
+          </p>
+          <p className="text-xs text-gray-500 group-hover:text-gray-700">
+            {user ? "View profile" : "Login"}
+          </p>
+        </div>
       )}
     </Link>
   );
