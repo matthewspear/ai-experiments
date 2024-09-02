3057f50bd34536b94b178cefcbfc17e74ea58c78
Matt Spear <matthewtimothyspear@gmail.com>
2024-09-02 14:57:05 +0800

    Test explict og title

--- src/app/(app)/tools/prompt-calculator/page.tsx
+++ src/app/(app)/tools/prompt-calculator/page.tsx
@@ -4,6 +4,11 @@ import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Prompt Calculator",
+  openGraph: {
+    title: "Prompt Calculator",
+    description:
+      "Calculate the cost of your prompts with the prompt calculator. Enter the input, output and model to get an estimate of the total cost.",
+  },
 };
 
 export default function Pricing() {
